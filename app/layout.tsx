335b2eedd38fdae13b40120ce7323e4df66cca11
Jurgen Brandstetter <jurgen@distill.fyi>
2024-10-27 22:08:49 +0100

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -70,6 +71,7 @@ export default function RootLayout({
             </div>
           </footer>
         </div>
+        <Analytics />
       </body>
     </html>
   );
